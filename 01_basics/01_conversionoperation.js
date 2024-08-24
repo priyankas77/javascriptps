119149d01a23ed8340f1ec3293bb43bc436369d2
PRIYANKA SHEE-CODER <142648256+priyankas77@users.noreply.github.com>
2024-08-24 05:22:13 +0000

    conversion type

---
+++ 01_basics/01_conversionoperation.js
@@ -0,0 +1,27 @@
+let score="33"
+console.log(typeof(score))
+console.log(typeof score);
+
+console.log(score);
+
+ let valueInNumber =Number(score)
+ console.log(typeof score);
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+ //now if we take 33abc it will tretaed as string but if we
+ //print out their value in number it will show NaN it means not a number
+
+
+ let isloggedIn =1
+ console.log(isloggedIn);
+ console.log(typeof isloggedIn);
+ let loggedIn =true 
+ console.log(typeof loggedIn);
+
+ //"33"=> 33
+ //"33abc"=>NaN
+ // true => boolean true=> 1 and false =>0
+ 
+ 
+ 
