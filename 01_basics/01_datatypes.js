d05419a9f0c792a1b7db93f5b81ccb6ec8b3af98
PRIYANKA SHEE-CODER <142648256+priyankas77@users.noreply.github.com>
2024-08-24 04:19:58 +0000

    container config done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,13 @@
+"use strict";
+let name= "Priyanka"
+//number 2 to power 53
+// BigInt 
+// String => " "
+// boolean =>true/false
+// null -> stand alone value which is empty 
+// undefined => no value given 
+// symbol => unique 
+// object
+console.log("Priyanka")
+console.log(typeof name)
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId=144553
+let accountEmail="priyankashee55@gmail.com"
+var accountPassword="12345"
+accountCity="Kolkata"
+console.log(accountId)
+let accountState
+console.table ([accountEmail,accountId,accountPassword,accountCity,accountState])
